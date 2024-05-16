22b067ded89a01ac114cd95aec5897073169f877
Rohith Kaki <rohithkaki11@gmail.com>
2024-05-16 09:10:28 +0530

    Add ToggleSwitch JS

---
+++ Projects/ToggleSwitch/script.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector("button");
+let body = document.querySelector("body");
+let mode = "light";
+
+btn.addEventListener("click", () =>{
+    if(mode === "light"){
+        mode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }else{
+        mode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+    console.log(mode);
+})
